b4640f17d73f14d12f4f874ab63365d540ae0eec
Siyi Guo <siyi.guo@grabtaxi.com>
2019-11-04 11:03:33 +0800

    (chore) 重命名文件和方法，使用包管理tree的内容

--- tree/node.go
+++ tree/node.go
@@ -1,50 +1,34 @@
-package main
+package tree
 
 import "fmt"
 
 // 定义结构
-type treeNode struct {
-	value       int
-	left, right *treeNode
+type Node struct {
+	Value       int
+	Left, Right *Node
 }
 
 // 值传递，会拷贝一份node
-func (node treeNode) print() {
-	fmt.Print(node.value)
+func (node Node) Print() {
+	fmt.Print(node.Value)
 }
 // 只有使用*才能修改值，因为值传递的原因，只能传指针进来才行
-func (node *treeNode) setValue(value int) {
-	node.value = value
+func (node *Node) SetValue(value int) {
+	node.Value = value
 }
 // node的中序遍历
-func (node *treeNode) traverse() {
+func (node *Node) Traverse() {
 	if node == nil {
 		return
 	}
-	node.left.traverse()
-	node.print()
-	node.right.traverse()
+	node.Left.Traverse()
+	node.Print()
+	node.Right.Traverse()
 }
 
 // 使用工厂函数代替构造函数
-func createNode(value int) *treeNode {
+func CreateNode(value int) *Node {
 	// 在go语言中，局部变量也可以返回给别人用，C++不行
 	// 不需要知道到底是分配到堆上还是栈上
-	return &treeNode{value: value} // 返回了局部变量的地址
-}
-
-func main() {
-	var root treeNode
-	root = treeNode{value: 3}
-	root.left = &treeNode{}
-	root.right = &treeNode{5, nil, nil}
-	root.right.left = new(treeNode)
-	nodes := []treeNode{
-		{value: 3},
-		{},
-		{6, nil, &root},
-	}
-	fmt.Println(nodes)
-	root.setValue(9)
-	root.print()
+	return &Node{Value: value} // 返回了局部变量的地址
 }
