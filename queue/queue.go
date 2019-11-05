de0fbfc080319738a2dc55caaf006a519f683b67
Siyi Guo <siyi.guo@grabtaxi.com>
2019-11-05 13:17:27 +0800

    (chore) 添加queue的实现

---
+++ queue/queue.go
@@ -0,0 +1,19 @@
+package queue
+
+// 使用别名的方式扩展类
+type Queue []int
+
+func (q *Queue) Push(v int) {
+	// 可以用指针的方式，改掉指针指向的对象
+	// 每次运算后，q会指向新的对象
+	*q = append(*q, v)
+}
+func (q *Queue) Pop() int {
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head
+}
+
+func (q *Queue)IsEmpty() bool {
+	return len(*q) == 0
+}
--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -4,6 +4,22 @@ import (
 	"../../tree"
 	"fmt"
 )
+// 使用组合的方式，扩展tree.Node
+type myNode struct {
+	node *tree.Node
+}
+
+func (mynode *myNode) postTraverse() {
+	if mynode == nil || mynode.node == nil {
+		return
+	}
+	left := myNode{mynode.node.Left}
+	left.postTraverse()
+	// 必须提成变量，否则无法取地址
+	right := myNode{mynode.node.Right}
+	right.postTraverse()
+	mynode.node.Print()
+}
 
 func main() {
 	var root tree.Node
@@ -19,4 +35,10 @@ func main() {
 	fmt.Println(nodes)
 	root.SetValue(9)
 	root.Print()
+	fmt.Println()
+	root.Traverse()
+	fmt.Println()
+	// 使用组合的方式
+	mynode := myNode{&root}
+	mynode.postTraverse()
 }
